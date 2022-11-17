e2fce19f062888e609421cc98c49f1954d50d372
yashodeepk <yrk9421@gmail.com>
2022-11-17 22:28:41 +0530

    added circular avatar

--- whatsapp_direct/lib/main.dart
+++ whatsapp_direct/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:call_log/call_log.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,36 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp Direct',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: Colors.green, scaffoldBackgroundColor: Colors.white),
+      home: const MyHomePage(title: 'Whatsapp Direct'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +33,191 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late Iterable<CallLogEntry> entries;
+  int callLogsTill = 5;
+  bool? fetching = true;
+  final _refreshKey = GlobalKey<RefreshIndicatorState>();
 
-  void _incrementCounter() {
+  void _printCallLogs(Iterable<CallLogEntry> entry) {
+    if (kDebugMode) {
+      print('Queried call log entries');
+      for (CallLogEntry entry in entries) {
+        print('-------------------------------------');
+        print('F. NUMBER  : ${entry.formattedNumber}');
+        print('C.M. NUMBER: ${entry.cachedMatchedNumber}');
+        print('NUMBER     : ${entry.number}');
+        print('NAME       : ${entry.name}');
+        print('TYPE       : ${entry.callType}');
+        print(
+            'DATE       : ${DateTime.fromMillisecondsSinceEpoch(entry.timestamp!)}');
+        print('DURATION   : ${entry.duration}');
+        print('ACCOUNT ID : ${entry.phoneAccountId}');
+        print('SIM NAME   : ${entry.simDisplayName}');
+        print('-------------------------------------');
+      }
+    }
+  }
+
+  Future<void> _getCalllogs() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      fetching = true;
+    });
+    var now = DateTime.now();
+    int from =
+        now.subtract(Duration(days: callLogsTill)).millisecondsSinceEpoch;
+    int to = now.millisecondsSinceEpoch;
+    entries = await CallLog.query(dateFrom: from, dateTo: to);
+    _printCallLogs(entries);
+    setState(() {
+      fetching = false;
     });
   }
 
   @override
+  void initState() {
+    _getCalllogs();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          entries.toList().isEmpty
+              ? fetching!
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Container()
+              : Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10.0, 2.0, 10.0, 2.0),
+                    child: SizedBox(
+                      height: 200.0,
+                      child: RefreshIndicator(
+                        key: _refreshKey,
+                        onRefresh: () async {
+                          await _getCalllogs();
+                        },
+                        child: ListView.builder(
+                            itemCount: entries.length,
+                            itemBuilder: (_, index) {
+                              return ContactCard(
+                                  fnumber: entries
+                                      .elementAt(index)
+                                      .formattedNumber
+                                      .toString(),
+                                  cmnumber: entries
+                                      .elementAt(index)
+                                      .cachedMatchedNumber
+                                      .toString(),
+                                  number: entries
+                                      .elementAt(index)
+                                      .number
+                                      .toString(),
+                                  name:
+                                      entries.elementAt(index).name.toString(),
+                                  type: entries
+                                      .elementAt(index)
+                                      .callType
+                                      .toString(),
+                                  date: entries
+                                      .elementAt(index)
+                                      .timestamp
+                                      .toString(),
+                                  duration: entries
+                                      .elementAt(index)
+                                      .duration
+                                      .toString(),
+                                  accountid: entries
+                                      .elementAt(index)
+                                      .phoneAccountId
+                                      .toString(),
+                                  simname: entries
+                                      .elementAt(index)
+                                      .simDisplayName
+                                      .toString());
+                            }),
+                      ),
+                    ),
+                  ),
+                ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _getCalllogs,
+        tooltip: 'Send',
+        child: const Icon(Icons.whatsapp_sharp),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ContactCard extends StatefulWidget {
+  ContactCard(
+      {Key? key,
+      required this.fnumber,
+      required this.cmnumber,
+      required this.number,
+      required this.name,
+      required this.type,
+      required this.date,
+      required this.duration,
+      required this.accountid,
+      required this.simname})
+      : super(key: key);
+
+  String fnumber;
+  String cmnumber;
+  String number;
+  String name;
+  String type;
+  String date;
+  String duration;
+  String accountid;
+  String simname;
+
+  @override
+  State<ContactCard> createState() => _ContactCardState();
+}
+
+class _ContactCardState extends State<ContactCard> {
+  static Color randomOpaqueColor() {
+    return Color(Random().nextInt(0xffffffff)).withAlpha(0xff);
+  }
+
+  String returnFirstNameChar(String name) {
+    return name.isEmpty ? 'U' : name[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+      child: Card(
+        elevation: 0,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Container(
+            alignment: Alignment.center,
+            child: CircleAvatar(
+              backgroundColor: Colors.green[
+                  ((returnFirstNameChar(widget.name).codeUnitAt(0)) %
+                          Colors.primaries.length) *
+                      100],
+              radius: 28,
+              child: Text(
+                returnFirstNameChar(widget.name),
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
